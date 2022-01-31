638943d6d0ca35899df4dc83a212547fb3785d80
kreptv <hrk6672@utulsa.edu>
2022-01-31 15:14:46 -0600

    Updated Unity Project

---
+++ ApplePicker-Unity/Assets/Scripts/AppleTree.cs
@@ -0,0 +1,63 @@
+/*
+ * Created by: Haley Kelly
+ * Date Created: 1/31/2022
+ * 
+ * Last Edited by: N/A
+ * Last Edited: 1/31/2022
+ * 
+ * Description: Controls movement of the AppleTree.
+ */
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppleTree : MonoBehaviour
+{
+
+    // Variables
+    [Header("Set in Inspector")]
+    public float speed = 1f; // tree speed
+    public float leftAndRightEdge = 10f; // distance where the tree turns around
+    public GameObject applePrefab; // prefab for instantiating apples
+    public float secondsBetweenApplesDropped = 1f; // time between apple drops
+    public float chanceToChangeDirection = 0.1f; // chance that the tree will change directions
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Basic Movement
+        Vector3 pos = transform.position; // records the current position
+        pos.x += speed * Time.deltaTime; // adds speed to x position
+        transform.position = pos; // apply the position value
+
+        // Change Direction
+        if(pos.x < -leftAndRightEdge)
+        {
+            speed = Mathf.Abs(speed); // set speed to positive
+        }
+        else if(pos.x > leftAndRightEdge)
+        {
+            speed = -Mathf.Abs(speed); // set speed to negative value
+        }
+
+    }
+
+    // FixedUpdate is called on fixed intervals (50 times per second)
+    private void FixedUpdate()
+    {
+        // Test chance of direction change
+        if (Random.value < chanceToChangeDirection)
+        {
+            speed *= -1; // change direction
+        }
+    }
+}
